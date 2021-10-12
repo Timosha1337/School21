db3c4f856f42a7fc10173fae2af6c83fdb42d71f
Timosha1337 <anna.assimpleassnow@gmail.com>
2021-10-13 01:35:49 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/libf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libf.dir/main.c.obj"
+  "libf.exe"
+  "libf.exe.manifest"
+  "libf.lib"
+  "libf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
