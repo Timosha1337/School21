4952f6fe15f6427543ef865638c820663029ba3e
Timosha1337 <anna.assimpleassnow@gmail.com>
2021-10-24 13:01:00 +0300

    + libft.h

--- main.c
+++ main.c
@@ -2,45 +2,10 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <unistd.h>
+#include "libtf.h"
 
 
-int     ft_isalpha(int argument);
-int     ft_isdigit( int arg );
-int     ft_isalnum(int argument);
-int     ft_isascii(int ch);
-int     ft_isprint( int arg );
-size_t  ft_strlen(const char *str);
-void *ft_memset(void *str, int c, size_t n);
-void ft_bzero(void *s, size_t n);
-void *ft_memcpy(void *dest, const void * src, size_t n);
-void *ft_memmove( void *str1, const void *str2, size_t n );
-size_t ft_strlcpy(char * dst, const char * src, size_t n);
-size_t ft_strlcat(char *dest, const char *src, size_t size);
-int ft_toupper(int c);
-int ft_tolower(int c);
-char *ft_strchr( const char * string, int symbol);
-char *ft_strrchr (const char *str, int ch);
-int ft_strncmp(const char *str1, const char *str2, size_t n);
-void *ft_memchr (const void *arr, int c, size_t n);
-int ft_memcmp (const void *arr1, const void *arr2, size_t n);
-char *ft_strstr(const char *scr, const char *sought);
-int ft_atoi (const char *str);
-void *ft_calloc(size_t num, size_t size);
-char *ft_strdup(const char *str);
-char *ft_substr(char const *s, unsigned int start, size_t len);
-char *ft_strjoin(char const *s1, char const *s2);
-char *ft_strtrim(char const *s1, char const *set);
-char **ft_split(char const *s, char c);
-int get_count_words(char const *s, char c);
-char *ft_itoa(int n);
-int count_numbers(int n);
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void ft_striteri(char *s, void (*f)(unsigned int, char*));
-void ft_putstr_fd(char *s, int fd);
-void ft_putendl_fd(char *s, int fd);
-void ft_putnbr_fd(int n, int fd);
 
 int main()
 {
